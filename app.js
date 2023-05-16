ba8b7b23f0f7c166bc2c6159d4029c149a710dfd
HammadShah <121276577+HammadShah90@users.noreply.github.com>
2023-05-16 08:55:08 +0500

    not completed

--- app.js
+++ app.js
@@ -15,19 +15,24 @@ const overlay = document.querySelector(".overlay");
 // --------Create Empty Array------->>>>
 
 let cartArticles = []
-// let itemUniqueId;
+var itemUniqueId;
+// console.log(itemUniqueId);
 
 
-// -----------Create Funstions---------->>>>
+
+// -----------Create Functions---------->>>>
+
 
 // ----------Alert Function------------>>>>
 
-const alertPara = (alertInnerText) => {
+const alertPara = (alertInnerText, action) => {
     alertText.innerHTML = alertInnerText
     alertText.style.visibility = "visible"
-
+    alertText.classList.add(`alert${action}`)
+    
     setTimeout(() => {
         alertText.style.visibility = "hidden"
+        alertText.classList.remove(`alert${action}`)
 
     }, 1500)
 }
@@ -52,46 +57,16 @@ const listAdd = () => {
     // console.log(inputText.value);
 
     if (inputText.value === "") {
-        alertPara(`Your Input is Empty! Please Write Something.`);
+        alertPara(`Your Input is Empty! Please Write Something.`, `Danger`);
         buttonTransform();
         return
     }
 
     buttonTransform();
 
-    let itemUniqueId = Date.now();
+    var itemUniqueId = Date.now();
 
     // console.log(itemUniqueId);
-
-    // let div = document.createElement("div");
-    // div.setAttribute("class", "item");
-    // div.setAttribute("id", `${itemUniqueId}`);
-    // let para = document.createElement("p");
-    // let paraText = document.createTextNode(`${inputText.value}`);
-    // para.appendChild(paraText);
-    // div.appendChild(para);
-    // let div2 = document.createElement("div");
-    // div2.setAttribute("class", "itemButtons");
-    // let editButton = document.createElement("button");
-    // editButton.setAttribute("class", "editItemButton");
-    // editButton.setAttribute("onclick", `editItem('${itemUniqueId}')`);
-    // let editButtonIcon = document.createElement("i");
-    // editButtonIcon.setAttribute("class", "fa-solid fa-pen-to-square");
-    // editButton.appendChild(editButtonIcon); 
-    // let deleteButton = document.createElement("button");
-    // deleteButton.setAttribute("class", "deleteItemButton"); 
-    // deleteButton.setAttribute("onclick", `deleteItem('${itemUniqueId}')`); 
-    // let deleteButtonIcon = document.createElement("i");
-    // deleteButtonIcon.setAttribute("class", "fa-solid fa-trash");
-    // deleteButton.appendChild(deleteButtonIcon);
-    // div2.appendChild(editButton) 
-    // div2.appendChild(deleteButton)
-    // div.appendChild(div2) 
-
-    // console.log(div);
-
-    // itemLists.appendChild(div)
-
 
     const itemArticleList = `<div id="${itemUniqueId}" class="item">
                                 <p>${inputText.value}</p>
@@ -106,17 +81,19 @@ const listAdd = () => {
                              </div>`;
     // console.log(itemArticleList);
 
+    clearAllButton.classList.remove("hidden")
+
     cartArticles.push(itemArticleList);
-    // console.log(cartArticles);
+    // console.log(typeof cartArticles.push(itemArticleList));
 
     itemLists.innerHTML = cartArticles.join("");
-    // console.log(cartArticles);
+    // console.log(typeof cartArticles.join(""));
 
-    // alertText.style.backgroundColor = "#1b9e01";
-
-    alertPara(`${inputText.value} is Added to your Grocery Cart`);
+    alertPara(`${inputText.value} is Added to your Grocery Cart`, `Success`);
 
     inputText.value = "";
+
+
 
 }
 
@@ -129,28 +106,21 @@ const editItem = (itemUId) => {
     editModaal.classList.remove("hidden")
     overlay.classList.remove("hidden")
 
-    // let modaalInputTag = document.createElement("input");
-    // modaalInputTag.setAttribute("type", "text");
-    // modaalInputTag.setAttribute("id", "modaalInput");
-    // let modaalButtonTag = document.createElement("button");
-    // modaalButtonTag.setAttribute("type", "submit");
-    // modaalButtonTag.setAttribute("id", "endEditing");
-    // modaalButtonTag.setAttribute("onclick", "editDoneButton()");
-    // let modaalButtonText= document.createTextNode("Done");
-    // modaalButtonTag.appendChild(modaalButtonText);
-    // editModaal.appendChild(modaalInputTag);
-    // editModaal.appendChild(modaalButtonTag);
 
+    const editMyItem = Array.from(itemLists.children);
 
-    // console.log(editModaal);
-
-    const editMyItem = Array.from(itemLists.childNodes);
+    // console.log(itemLists.children[itemUId].id);
 
     const filteredData = editMyItem.filter((singleItem) => singleItem.id === itemUId)
 
-    modaalInput.value = filteredData[0].querySelector('p').innerHTML
+    // console.log(filteredData[0].querySelector('p').innerHTML);
 
+    modaalInput.value = filteredData[0].querySelector('p').innerHTML
+    
+    
     // console.log(filteredData[0].querySelector('p').innerHTML)
+    
+    
 }
 
 
@@ -159,59 +129,67 @@ const editItem = (itemUId) => {
 const editDoneButton = () => {
     editModaal.classList.add("hidden")
     overlay.classList.add("hidden")
-
-    console.log(modaalInput);
-
-    const editMyItem = Array.from(itemLists.childNodes);
-
-    const filteredData = editMyItem.filter((singleItem) => singleItem.id)
-
-    console.log(filteredData[0].querySelector('p'));
-
-    filteredData[0].querySelector('p').innerHTML = modaalInput.value
-
-    alertPara(`${inputText.value} item is Edited`);
+    
+    console.log(modaalInput.value);
+    
+    // console.log(itemUId);
+    
+    // console.log(itemLists.children[0]);
+    
+    // const editMyItem = Array.from(itemLists.children);
+    
+    // console.log(editMyItem[0]);
+    
+    // const filteredData2 = filteredData.find((singleItem) => singleItem.id === itemUId)
+    
+    // console.log(filteredData2);
+    
+    // filteredData[0].querySelector('p').innerHTML = modaalInput.value
+    
+    // alertPara(`${inputText.value} item is Edited`, `Success`);
 }
+
 
 // ----------Delete Button Function------------>>>>
 
 const deleteItem = (itemUId) => {
     console.log("Deleted Item ID", itemUId);
-
+    
     const findIndexNum = cartArticles.findIndex((perItem) => perItem.includes(itemUId));
-
+    
     console.log(findIndexNum);
-
+    
     cartArticles.splice(findIndexNum, 1);
-
+    
     itemLists.innerHTML = cartArticles.join("");
+    
+    alertPara(`Item is deleted`, `Danger`);
+    
+    if (findIndexNum == "0") {
 
-    alertPara(`${inputText.value} item is deleted`);
+        clearAllButton.classList.add("hidden")
+
+    }
 }
 
 
 // ----------Clear All Button Function------------>>>>
 
 const clearAll = () => {
-
+    
     // console.log("chal rha hai");
-
-    if (itemLists.innerHTML === "") {
-        alertPara(`Item List already Empty`);
-        return
-    }
-
-    // alertText.style.backgroundColor = "red";
-
+    
     cartArticles = [];
-
+    
     itemLists.innerHTML = "";
-
+    
     inputText.value = "";
-
-
-    alertPara(`All Items are Cleared`)
-
+    
+    
+    alertPara(`All Items are Cleared`, `Danger`)
+    
+    clearAllButton.classList.add("hidden")
+    
 }
 
 
